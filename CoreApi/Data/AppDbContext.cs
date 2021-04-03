b7b98f1b477f7fab48343b90ca8145d97fb042c0
DangToan <dtoan2805@gmail.com>
2021-04-03 02:09:01 -0700

    Add DbContext Class

---
+++ CoreApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApi.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
